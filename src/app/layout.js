4e2d27ada0ab66d87c6dbf35610442400305014a
Patricio Mena <luispatricio.mh@gmail.com>
2025-09-27 20:24:07 -0600

    new branch

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,14 @@ import React from 'react';
 import './styles.css';
 
 function RootLayout({ children }) {
+  console.log('Layout rendered');
+  const timeStamp = new Date().toLocaleString();
+
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>Page rendered on {timeStamp}</footer>
       </body>
     </html>
   );
